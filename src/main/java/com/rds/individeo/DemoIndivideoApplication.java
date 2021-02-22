12434077e0d3b2477f902e74562e6f42bbda0bd7
nanda <nandaceria@gmail.com>
2021-02-22 10:10:40 +0700

    remove all setting

--- src/main/java/com/rds/individeo/DemoIndivideoApplication.java
+++ src/main/java/com/rds/individeo/DemoIndivideoApplication.java
@@ -2,12 +2,19 @@ package com.rds.individeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoIndivideoApplication {
+public class DemoIndivideoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoIndivideoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoIndivideoApplication.class);
 	}
 
 }
